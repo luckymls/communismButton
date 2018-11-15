07486890bd47cf2278d11cc682e362ed001f1e39
luckymls <melis.luca2014@gmail.com>
2018-11-15 05:03:12 +0100

    Update button.py

--- button.py
+++ button.py
@@ -1,20 +1,12 @@
-from tkinter import *
-
-root = Tk()
-root.geometry("400x400")
-
-statoPulsante = IntVar()
-
-
-Pulsante = PhotoImage(file=r"C:\Users\alber\Desktop\400_ombra.png")
+pulsantePic = PhotoImage(file='400_ombra.png')
 
 b=Button(root)
 b.pack(expand=True)
-b.config(image=Pulsante, command=lambda: changeImage(b))
+b.config(image=pulsantePic, command=lambda: changeImage(b))
 
 def changeImage(button=None):
 
     
-    pulsantePic = PhotoImage(file=r"C:\Users\alber\Desktop\400_premuto.png")
+    pulsantePic = PhotoImage(file='400_premuto.png')
     button.config(image=pulsantePic)
-    # onAction()
+    onAction()
