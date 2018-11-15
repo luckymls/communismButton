c52247be29763eb42276e259c82f334b29d961b6
Zeroandahalf <45055197+Zeroandahalf@users.noreply.github.com>
2018-11-15 05:01:23 +0100

    Prima veri

--- button_act.py
+++ button_act.py
@@ -1,1 +1,23 @@
+from pygame import mixer
+from tkinter import *
 
+root = Tk()
+statoBottone = IntVar()
+
+
+def onAction (event=None): # Richiano funzione con file in riproduzione
+    if statoBottone.get(): # Se il bottone è attivo
+        statoBottone.set(0) # Il bottone è stato usato
+        innoStop()
+    else:
+        statoBottone.set(1)
+        innoStart()
+    
+def innoStart (): # Richiamo file
+    mixer.init()
+    mixer.music.load('C:/Users/Luca Corona/Downloads/soviet-anthem1944.mp3')
+    mixer.music.play()
+
+def innoStop ():
+    mixer.init()
+    mixer.music.stop()
